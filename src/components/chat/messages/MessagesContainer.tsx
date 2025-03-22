16ee58ccf67f7ce1da43e1ab0202eaefa85c863f
Hades <jessay@gmail.com>
2025-03-22 14:08:46 -0700

    alignment

--- src/components/chat/messages/MessagesContainer.tsx
+++ src/components/chat/messages/MessagesContainer.tsx
@@ -1,1 +1,24 @@
- 
+import type { Message } from "@/types/chat";
+import React from "react";
+import { MessageItem } from "./MessageItem";
+import { MessagePreferencesProvider } from "./MessagePreferences";
+
+interface MessagesContainerProps {
+  messages: Message[];
+  className?: string;
+}
+
+export const MessagesContainer: React.FC<MessagesContainerProps> = ({
+  messages,
+  className,
+}) => {
+  return (
+    <MessagePreferencesProvider>
+      <div className={className}>
+        {messages.map((message) => (
+          <MessageItem key={message.id} message={message} />
+        ))}
+      </div>
+    </MessagePreferencesProvider>
+  );
+};
