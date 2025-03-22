2cad11377772f8ae2a2e51d389634e63d7f8b792
Hades <jessay@gmail.com>
2025-03-22 12:33:17 -0700

    fixing imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./components/chat/styles/animations.css";
 import "./components/chat/styles/chat-base.css";
+import "./components/chat/styles/chat-variables.css";
 import "./components/chat/styles/cyber-theme.css";
 import "./index.css";
 
