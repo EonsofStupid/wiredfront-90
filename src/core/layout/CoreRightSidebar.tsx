c579c203e540b38821cc9aef4a7b260173b85906
Hades <jessay@gmail.com>
2025-03-24 10:39:56 -0700

    fixing imports

--- src/core/layout/CoreRightSidebar.tsx
+++ src/core/layout/CoreRightSidebar.tsx
@@ -1,24 +1,26 @@
-
-import React from "react";
-import { cn } from "@/lib/utils";
-import { SidebarProps, LayoutZIndex } from "./types";
 import { ProjectOverview } from "@/components/layout/Sidebar/ProjectOverview";
+import { cn } from "@/lib/utils";
+import { LayoutZIndex, SidebarProps } from "./types";
 
 /**
  * @name CoreRightSidebar
  * @description The right sidebar for project overview
  * DO NOT MODIFY THIS COMPONENT
  */
-export function CoreRightSidebar({ className, isCompact, isVisible }: Omit<SidebarProps, 'side'> & { isVisible: boolean }) {
+export function CoreRightSidebar({
+  className,
+  isCompact,
+  isVisible,
+}: Omit<SidebarProps, "side"> & { isVisible: boolean }) {
   return (
-    <div 
+    <div
       className={cn(
         "wf-core-rightsidebar fixed right-0 top-16 bottom-12 glass-card border-l border-neon-blue/20 transition-all duration-300",
         isVisible ? "translate-x-0" : "translate-x-full",
         "w-64",
         className
       )}
-      style={{ zIndex: LayoutZIndex.projectHub }}
+      style={{ zIndex: LayoutZIndex.projecthub }}
       data-testid="core-rightsidebar"
     >
       <ProjectOverview isCompact={isCompact} className="h-full" />
